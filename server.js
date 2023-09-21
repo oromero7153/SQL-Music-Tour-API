8fbf3ea4e880afcfcf1190e15edbfa27bd0fe2d0
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-09-20 21:29:09 -0700

    added the migration files to the database

--- server.js
+++ server.js
@@ -8,21 +8,6 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// SEQUELIZE CONNECTION
-const sequelize = new Sequelize({
-    storage: process.env.PG_URI,
-    dialect: 'postgres',
-    username: 'postgres',
-    password: 'Moc#1907'
-  })
-  
-
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
 
 // ROOT
 app.get('/', (req, res) => {
