276f0fede493ca7daee7b054e3fa7dae35a2ba4c
oromero7153 <131738715+oromero7153@users.noreply.github.com>
2023-09-23 13:41:32 -0700

    added queries to the code

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,12 +2,18 @@
 const bands = require('express').Router()
 const db = require('../models')
 const { Band } = db
+const { Op } = require ('sequelize')
 
 //Find all bands
 // GET http://localhost:300/bands
 bands.get('/', async (req, res)=> {
     try {
-        const foundBands = await Band.findAll()
+        const foundBands = await Band.findAll({
+            order: [ [ 'available_start_time', 'ASC']],
+            where: {
+                name:{ [Op.like]: `%${req.query.name ? req.query.name: '' }%`}
+            }
+        })
         res.status(200).json(foundBands)
     } catch (error) {
         rest.status(500).json(error)
